1d13b1cfc13f9d286aa7ac7edddca8e2358d3b84
Zainab Jimoh <jimohzainab4753@gmail.com>
2019-10-22 15:24:38 +0000

    Added splash activity

--- app/src/main/java/com/example/carpark/views/SplashActivity.java
+++ app/src/main/java/com/example/carpark/views/SplashActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.carpark.R;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
