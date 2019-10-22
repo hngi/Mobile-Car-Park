6693c991edd9b42919afd70b651fbda59fd0238f
Tobby Junior <umohjunior96@gmail.com>
2019-10-23 00:21:37 +0100

    add enterName activity and java file

---
+++ app/src/main/java/com/example/carpark/EnterNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.carpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_name);
+    }
+}
