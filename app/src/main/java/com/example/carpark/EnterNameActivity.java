99c725168b92e01c51bb71c4b4f4b7261d8d0c39
Tobby Junior <umohjunior96@gmail.com>
2019-10-23 00:44:36 +0100

    design and write logic for EnterN

--- app/src/main/java/com/example/carpark/EnterNameActivity.java
+++ app/src/main/java/com/example/carpark/EnterNameActivity.java
@@ -1,14 +1,61 @@
 package com.example.carpark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.carpark.views.MainActivity;
 
 public class EnterNameActivity extends AppCompatActivity {
+
+
+    EditText firstName,lastName;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_name);
+
+
+    firstName = findViewById(R.id.firstName);
+    lastName = findViewById(R.id.lastName);
+    btnContinue = findViewById(R.id.btnContinue);
+
+    Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.app_name));
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            continueButton();
+        }
+    });
+
+}
+
+
+
+    public void continueButton(){
+        String firstName = this.firstName.getText().toString();
+        String lastName = this.lastName.getText().toString();
+
+
+
+        if (firstName.isEmpty()) {
+            this.firstName.setError("Please enter your first name");
+        } else if (lastName.isEmpty()) {
+            this.lastName.setError("Please enter your last name");
+        }else{
+            Intent intent = new Intent(EnterNameActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
