a6c7521db9532866061735f505a4cb740c67f0a0
Noel-elon <noelnwaelugo@gmail.com>
2019-10-23 14:02:18 +0100

    fixed details activity

--- app/src/main/java/com/example/carpark/views/SplashActivity.java
+++ app/src/main/java/com/example/carpark/views/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), OnboardingActivity.class);
+                Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
                 startActivity(intent);
                 finish();
             }
