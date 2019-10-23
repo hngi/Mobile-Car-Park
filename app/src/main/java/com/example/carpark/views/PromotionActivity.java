86db5b859448b592d521450e2314a10f212d6245
Rita <ritaokonkwo6@gmail.com>
2019-10-23 11:59:36 +0100

    XML and Java for Promotion Page

--- app/src/main/java/com/example/carpark/views/PromotionActivity.java
+++ app/src/main/java/com/example/carpark/views/PromotionActivity.java
@@ -3,14 +3,35 @@ package com.example.carpark.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.carpark.R;
 
 public class PromotionActivity extends AppCompatActivity {
+    EditText rectangle;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
+
+        //change action bar title
+        getSupportActionBar().setTitle("Apply Promo Code");
+
+        //find views by id
+        rectangle = findViewById(R.id.promotion_rectangle);
+        btn = findViewById(R.id.promo_btn);
+
+        //set on click listener on button
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Promo button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
