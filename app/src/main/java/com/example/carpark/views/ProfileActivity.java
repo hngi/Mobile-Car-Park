a03572125ef99d37d35a195d5c3469e7d5d6f320
Adetoun O <35571512+Adetohun@users.noreply.github.com>
2019-10-23 12:09:51 -0700

    Add files via upload

--- app/src/main/java/com/example/carpark/views/ProfileActivity.java
+++ app/src/main/java/com/example/carpark/views/ProfileActivity.java
@@ -2,48 +2,15 @@ package com.example.carpark.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.carpark.MyVehicleActivity;
 import com.example.carpark.R;
-import com.google.android.material.textfield.TextInputLayout;
 
-/**
- * Created by Utibe Etim
- * etim.utibe@gmail.com
- * 23 Oct, 2019
- */
-public class ProfileActivity extends BaseActivity {
-
-    private TextInputLayout fname_edt, lname_edt, phone_edt, email_edt;
-    private Button save_btn;
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        initView();
-    }
-
-    private void initView() {
-        fname_edt = findViewById(R.id.profile_fname_edtv);
-        lname_edt = findViewById(R.id.profile_lname_edtv);
-        phone_edt = findViewById(R.id.profile_phone_edtv);
-        email_edt = findViewById(R.id.profile_email_edtv);
-        save_btn = findViewById(R.id.profile_save_btn);
-
-        save_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ProfileActivity.this, MyVehicleActivity.class);
-                showToast("Your information has been saved successfully");
-                startActivity(intent);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/carpark/views/PromotionActivity.java
+++ app/src/main/java/com/example/carpark/views/PromotionActivity.java
@@ -2,9 +2,7 @@ package com.example.carpark.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,15 +30,7 @@ public class PromotionActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getApplicationContext(), "Promo button clicked", Toast.LENGTH_SHORT).show();
-                String editText = rectangle.getText().toString();
-                if(TextUtils.isEmpty(editText)){
-                    rectangle.setError("Please enter a promotion code");
-                    rectangle.requestFocus();
-                    return;
-                }
-                Intent intent = new Intent(PromotionActivity.this, PaymentMethodsActivity.class);
-                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Promo button clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
