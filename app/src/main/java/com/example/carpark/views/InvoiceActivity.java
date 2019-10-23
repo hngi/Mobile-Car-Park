0f70daf63f4fffc4a7b46cbece660fb04b99e4a0
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2019-10-23 20:23:28 +0100

    Added some minor changes

--- app/src/main/java/com/example/carpark/views/InvoiceActivity.java
+++ app/src/main/java/com/example/carpark/views/InvoiceActivity.java
@@ -1,6 +1,7 @@
 package com.example.carpark.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,9 @@ public class InvoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoice);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Invoice");
     }
 }
