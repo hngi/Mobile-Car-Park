d6037f887f4ef2d954ccbaf8946a6df4853c54cc
Robin <emeka14real@gmail.com>
2019-10-28 14:40:07 +0100

    Finished everything

--- app/src/main/java/com/example/carpark/views/HomeActivity.java
+++ app/src/main/java/com/example/carpark/views/HomeActivity.java
@@ -1,25 +1,17 @@
 package com.example.carpark.views;
-
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-
 import com.example.carpark.R;
 import com.example.carpark.views.homefragments.DefaultFragment;
 import com.example.carpark.views.homefragments.MyVehicleFragment;
@@ -120,10 +112,6 @@ public class HomeActivity extends AppCompatActivity {
                     setUpFragment(fragment);
                     enableBackViews(true);
                     toolbar.setTitle(titile);
-
-
-
-
                 }
                 return true;
             }
@@ -158,8 +146,8 @@ public class HomeActivity extends AppCompatActivity {
             final Drawable upArrow = getResources().getDrawable(R.drawable.ic_action_back);
             upArrow.setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_ATOP);
             getSupportActionBar().setHomeAsUpIndicator(upArrow);
-
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
             toolbar.setTitleTextColor(getResources().getColor(R.color.white));
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
