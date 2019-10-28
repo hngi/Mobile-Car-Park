a1182735b3c9c25505942e6575b991102a32375a
paulextrawonder <paulextrawonder@gmail.com>
2019-10-28 21:41:39 +0100

    Merge branch 'dev_test' of https://github.com/hngi/Mobile-Car-Park into My_Vehicle_Screen
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/carpark/views/HomeActivity.java
+++ app/src/main/java/com/example/carpark/views/HomeActivity.java
@@ -1,21 +1,16 @@
 package com.example.carpark.views;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -28,6 +23,8 @@ import com.example.carpark.views.homefragments.PaymentMethodsFragment;
 import com.example.carpark.views.homefragments.PromotionFragment;
 import com.example.carpark.views.homefragments.SettingsFragment;
 import com.google.android.material.navigation.NavigationView;
+
+;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -161,7 +158,7 @@ public class HomeActivity extends AppCompatActivity {
 
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+            toolbar.setTitleTextColor(getResources().getColor(R.color.color_white));
             toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
             if (!mToolBarNavigationListenerIsRegistered) {
@@ -182,7 +179,7 @@ public class HomeActivity extends AppCompatActivity {
             toggle.setToolbarNavigationClickListener(null);
             mToolBarNavigationListenerIsRegistered = false;
 
-            toolbar.setBackgroundColor(getResources().getColor(R.color.white));        }
+            toolbar.setBackgroundColor(getResources().getColor(R.color.color_white));        }
     }
 
 }
