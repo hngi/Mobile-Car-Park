276e246d3238dfa4f2d23d82e3e2094286608369
Rita <ritaokonkwo6@gmail.com>
2019-10-31 08:55:58 +0100

    random

--- app/src/main/java/com/example/carpark/Api/Api.java
+++ app/src/main/java/com/example/carpark/Api/Api.java
@@ -1,5 +1,7 @@
 package com.example.carpark.Api;
 
+import com.example.carpark.Model.User;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
--- app/src/main/java/com/example/carpark/Model/User.java
+++ app/src/main/java/com/example/carpark/Model/User.java
@@ -1,4 +1,52 @@
 package com.example.carpark.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+    @SerializedName("otp")
+    @Expose
+    private String otp;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
