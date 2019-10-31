da44f3cdb44a9c0c8838aa85caf1137024b9abed
Tobby Junior <umohjunior96@gmail.com>
2019-10-31 19:55:09 +0100

    add ids and link car details activity to schedule activity

--- app/src/main/java/com/example/carpark/views/ScheduleActivity.java
+++ app/src/main/java/com/example/carpark/views/ScheduleActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -25,6 +26,7 @@ public class ScheduleActivity extends AppCompatActivity {
     private TextView tvCheckIn;
     private TextView tvCheckOut;
     private TextView tvDuration;
+    Button add_vehicle;
     final Calendar checkInDate = Calendar.getInstance();
     final Calendar checkOutDate = Calendar.getInstance();
 
@@ -39,6 +41,16 @@ public class ScheduleActivity extends AppCompatActivity {
         tvCheckIn = findViewById(R.id.textView7);
         tvCheckOut = findViewById(R.id.textView10);
         tvDuration = findViewById(R.id.textView13);
+        add_vehicle = findViewById(R.id.fb_schedule_add);
+
+
+        add_vehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScheduleActivity.this,CarDetailsActiviy.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
