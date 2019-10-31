469526cf45411f4882daf4184dc65d383919e600
Robin <emeka14real@gmail.com>
2019-10-31 07:01:45 +0100

    Finished Finished Interface

--- app/src/main/java/service/ApiService.java
+++ app/src/main/java/service/ApiService.java
@@ -1,5 +1,14 @@
 package service;
 
+import com.example.carpark.Api.Responses.BaseDataResponse;
+import com.example.carpark.Api.Responses.BaseResponse;
+import com.example.carpark.Api.Responses.LoginReg.UserResponse;
+import com.example.carpark.Model.NewUser;
+import com.example.carpark.Model.PhoneOtp;
+import com.example.carpark.Model.User;
+import com.example.carpark.Model.UserProfile;
+import com.example.carpark.Model.Vehicle;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -14,6 +23,7 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
+    // Registration
     @POST("auth/register")
     Call<UserResponse> registerUser(@Body NewUser newUser);
 
@@ -22,16 +32,14 @@ public interface ApiService {
     Call<BaseDataResponse<BaseResponse>>sendOTP(@Field("phone") String phone);
 
     @POST("auth/verify-otp")
-    @FormUrlEncoded
-    Call<BaseDataResponse<BaseResponse>> verifyOTP(@Field("phone") String phone, @Field("otp") String otp);
+    Call<BaseDataResponse<BaseResponse>> verifyOTP(@Body PhoneOtp phoneOtp);
 
     @POST("auth/send-otp")
     @FormUrlEncoded
-    Call<BaseDataResponse<BaseResponse>> loginUser(@Field("phone") String phone, @Query ("login") String status);
+    Call<BaseDataResponse<BaseResponse>> loginUser(@Field("phone") String phone, @Query ("login") boolean status);
 
     @POST("auth/login")
-    @FormUrlEncoded
-    Call<BaseDataResponse<BaseResponse>> getUserAcessToken(@Field("phone") String phone, @Field("otp") String otp);
+    Call<BaseDataResponse<BaseResponse>> getUserAcessToken(@Body PhoneOtp phoneOtp);
 
     @GET("vehicles")
     Call<Vehicle> listAllVehicles();
@@ -49,10 +57,9 @@ public interface ApiService {
     Call<Void> deleteVehicleRecord(@Path("id") int id, @Field("plate_number") String plate_number, @Field("make_model") String make_model);
 
     @GET("user")
-    Call<Vehicle> listAllVehicles();
+    Call<User> getUserProfile();
 
     @PATCH("user")
-    @FormUrlEncoded
-    Call<BaseDataResponse<Vehicle>> editVehicleRecord(@Body UserProfile userProfile);
+    Call<BaseDataResponse<User>> editVehicleRecord(@Body UserProfile userProfile);
 
 }
