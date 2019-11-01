0522c77f4e4d1b86a7185c2adb7d4f409a90d000
Rita <ritaokonkwo6@gmail.com>
2019-11-01 18:54:38 +0100

    search for any location in home activity map

--- app/src/main/java/com/example/carpark/views/homefragments/DefaultFragment.java
+++ app/src/main/java/com/example/carpark/views/homefragments/DefaultFragment.java
@@ -2,59 +2,99 @@ package com.example.carpark.views.homefragments;
 
 
 import android.Manifest;
+
 import android.content.Intent;
+
 import android.content.pm.PackageManager;
+
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+
 import android.os.Build;
+
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+
+
 import androidx.core.app.ActivityCompat;
+
 import androidx.core.content.ContextCompat;
+
 import androidx.fragment.app.Fragment;
 
+
+
 import android.util.Log;
+
 import android.view.LayoutInflater;
+
 import android.view.View;
+
 import android.view.ViewGroup;
+
 import android.widget.Button;
+
 import android.widget.EditText;
+
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
 import android.widget.Toast;
 
+
+
 import com.example.carpark.Model.map.GetNearbyPlacesData;
+
 import com.example.carpark.R;
+
 import com.example.carpark.views.DetailsActivity;
-import com.example.carpark.views.MapsActivity;
+
 import com.google.android.gms.common.ConnectionResult;
+
 import com.google.android.gms.common.GoogleApiAvailability;
+
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
+
 import com.google.android.gms.location.LocationListener;
+
 import com.google.android.gms.location.LocationRequest;
+
 import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.maps.CameraUpdate;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
 import com.google.android.gms.maps.model.LatLng;
+
 import com.google.android.gms.maps.model.LatLngBounds;
+
 import com.google.android.gms.maps.model.Marker;
+
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.facebook.GraphRequest.TAG;
 
-public class DefaultFragment extends Fragment implements OnMapReadyCallback {
+public class DefaultFragment extends Fragment implements OnMapReadyCallback,
+
+        GoogleApiClient.ConnectionCallbacks,
+
+        GoogleApiClient.OnConnectionFailedListener,
+
+        GoogleMap.OnMarkerClickListener,
+
+        LocationListener {
 
 
 
@@ -63,6 +103,42 @@ public class DefaultFragment extends Fragment implements OnMapReadyCallback {
     private LinearLayout homeSchedule;
 
     private LinearLayout homeParkingSpace;
+
+    private static final LatLng Ikeja_G = new LatLng(6.590828, 3.342363);
+
+    private static final LatLng N_Air = new LatLng(6.572047, 3.322867);
+
+    private static final LatLng Ikeja_p = new LatLng(6.595741, 3.337888);
+
+
+
+
+
+    private Marker mIkeja_g;
+
+    private Marker mN_air;
+
+    private Marker mIkeja_p;
+
+    private Button schedule;
+
+    private EditText input_search;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    double latitude;
+
+    double longitude;
+
+    private int PROXIMITY_RADIUS = 10000;
+
+    Location mLastLocation;
+
+    Marker mCurrLocationMarker;
+
+    LocationRequest mLocationRequest;
+
+    View root;
 
 
 
@@ -78,7 +154,29 @@ public class DefaultFragment extends Fragment implements OnMapReadyCallback {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.content_home, container, false);
+        root = inflater.inflate(R.layout.content_home, container, false);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            checkLocationPermission();
+
+        }
+
+
+
+        //Check if Google Play Services Available or not
+
+        if (!CheckGooglePlayServices()) {
+
+            Log.d("onCreate", "Finishing test case since Google Play Services are not available");
+
+        }
+
+        else {
+
+            Log.d("onCreate","Google Play Services available.");
+
+        }
 
 
 
@@ -96,9 +194,237 @@ public class DefaultFragment extends Fragment implements OnMapReadyCallback {
 
         Toast.makeText(getActivity(), "Please click on schedule to test other activities", Toast.LENGTH_LONG).show();
 
+        ImageView imageView = root.findViewById(R.id.ic_magnify);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSearch(view);
+            }
+        });
+
 
 
         return root;
+
+    }
+
+    private boolean CheckGooglePlayServices() {
+
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+
+        int result = googleAPI.isGooglePlayServicesAvailable(getContext());
+
+        if(result != ConnectionResult.SUCCESS) {
+
+            if(googleAPI.isUserResolvableError(result)) {
+
+                googleAPI.getErrorDialog(getActivity(), result,
+
+                        0).show();
+
+            }
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+
+
+
+
+
+
+    /** Called when the map is ready. */
+
+    @Override
+
+    public void onMapReady(GoogleMap map) {
+
+        mMap = map;
+
+        //Initialize Google Play Services
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(getContext(),
+
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                buildGoogleApiClient();
+
+                mMap.setMyLocationEnabled(true);
+
+            }
+
+        }
+
+        else {
+
+            buildGoogleApiClient();
+
+            mMap.setMyLocationEnabled(true);
+
+        }
+
+        mMap.clear();
+
+
+
+
+
+        // Add some markers to the map, and add a data object to each marker.
+
+        mIkeja_g = mMap.addMarker(new MarkerOptions()
+
+                .position(Ikeja_G)
+
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_park))
+
+                .title("Ikeja General Hospital, Car Park"));
+
+        mIkeja_g.setTag(0);
+
+
+
+        mN_air = mMap.addMarker(new MarkerOptions()
+
+                .position(N_Air)
+
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_park))
+
+                .title("New Airport, Car Park"));
+
+        mN_air.setTag(0);
+
+
+
+        mIkeja_p = mMap.addMarker(new MarkerOptions()
+
+                .position(Ikeja_p)
+
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_park))
+
+                .title("Ikeja, Car Park"));
+
+        mIkeja_p.setTag(0);
+
+
+
+        //LatLngBound will cover all your marker on Google Maps
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+
+
+//the include method will calculate the min and max bound.
+
+        builder.include(mIkeja_g.getPosition());
+
+        builder.include(mN_air.getPosition());
+
+        builder.include(mIkeja_p.getPosition());
+
+
+
+        LatLngBounds bounds = builder.build();
+
+
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+
+        int height = getResources().getDisplayMetrics().heightPixels;
+
+        int padding = (int) (width * 0.10); // offset from edges of the map 10% of screen
+
+
+
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+
+
+
+        mMap.animateCamera(cu);
+
+
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+
+                .addConnectionCallbacks(this)
+
+                .addOnConnectionFailedListener(this)
+
+                .addApi(LocationServices.API)
+
+                .build();
+
+        mGoogleApiClient.connect();
+
+    }
+
+    public void onSearch(View view){
+        input_search = root.findViewById(R.id.input_search);
+            String location = input_search.getText().toString();
+             List<Address> addressList = null;
+
+            if(location != null || !location.equals("")){
+                Geocoder geocoder = new Geocoder(getContext());
+
+                try {
+                     addressList = geocoder.getFromLocationName(location, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+    }
+
+
+
+    @Override
+
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+
+        mLocationRequest.setInterval(1000);
+
+        mLocationRequest.setFastestInterval(1000);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+                == PackageManager.PERMISSION_GRANTED) {
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        }
+
+    }
+
+
+
+
+
+    @Override
+
+    public void onConnectionSuspended(int i) {
+
+
 
     }
 
@@ -106,25 +432,253 @@ public class DefaultFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
 
-    public void onMapReady(GoogleMap googleMap) {
+    public void onLocationChanged(Location location) {
+
+        Log.d("onLocationChanged", "entered");
 
 
 
-        mMap = googleMap;
+        mLastLocation = location;
+
+        if (mCurrLocationMarker != null) {
+
+            mCurrLocationMarker.remove();
+
+        }
 
 
 
-        // Add a marker in Sydney and move the camera
+        //Place current location marker
 
-        LatLng sydney = new LatLng(6.605874, 3.349149);
+        latitude = location.getLatitude();
 
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Ikeja"));
+        longitude = location.getLongitude();
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(latLng);
+
+        markerOptions.title("Current Position");
+
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+
+
+        getNearbyParks();
+
+        mMap.setOnMarkerClickListener(this);
+
+
+
+
+
+
+
+        //move map camera
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+
+
+        Log.d("onLocationChanged", String.format("latitude:%.3f longitude:%.3f",latitude,longitude));
+
+
+
+        //stop location updates
+
+        if (mGoogleApiClient != null) {
+
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+
+            Log.d("onLocationChanged", "Removing Location Updates");
+
+        }
+
+        Log.d("onLocationChanged", "Exit");
 
 
 
     }
+
+
+
+    private void getNearbyParks() {
+
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+
+        googlePlacesUrl.append("location=" + latitude + "," + longitude);
+
+        googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
+
+        googlePlacesUrl.append("&type=parking");
+
+        googlePlacesUrl.append("&sensor=true");
+
+        googlePlacesUrl.append("&key=" + "AIzaSyD1XwtM5P9rGEwSyvDY4f2W5VqhS85Xdjo");
+
+        String url = googlePlacesUrl.toString();
+
+        Object dataTransfer[] = new Object[2];
+
+        dataTransfer[0]= mMap;
+
+        dataTransfer[1]= url;
+
+        GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+        getNearbyPlacesData.execute(dataTransfer);
+
+    }
+
+
+
+    @Override
+
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+
+
+    }
+
+
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission(){
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+
+            // Asking user if explanation is needed
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+
+                // Show an explanation to the user *asynchronously* -- don't block
+
+                // this thread waiting for the user's response! After the user
+
+                // sees the explanation, try again to request the permission.
+
+
+
+                //Prompt the user once explanation has been shown
+
+                ActivityCompat.requestPermissions(getActivity(),
+
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(getActivity(),
+
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+            }
+
+            return false;
+
+        } else {
+
+            return true;
+
+        }
+
+    }
+
+
+
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode,
+
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+
+                // If request is cancelled, the result arrays are empty.
+
+                if (grantResults.length > 0
+
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+
+                    // permission was granted. Do the
+
+                    // contacts-related task you need to do.
+
+                    if (ContextCompat.checkSelfPermission(getContext(),
+
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+
+                            == PackageManager.PERMISSION_GRANTED) {
+
+
+
+                        if (mGoogleApiClient == null) {
+
+                            buildGoogleApiClient();
+
+                        }
+
+                        mMap.setMyLocationEnabled(true);
+
+                    }
+
+
+
+                } else {
+
+
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+
+                    Toast.makeText(getActivity(), "permission denied", Toast.LENGTH_LONG).show();
+
+                }
+
+                return;
+
+            }
+
+
+
+            // other 'case' lines to check for other permissions this app might request.
+
+            // You can add here other case statements according to your requirement.
+
+        }
+
+    }
+
+
 
 
 
@@ -169,5 +723,35 @@ public class DefaultFragment extends Fragment implements OnMapReadyCallback {
     }
 
 
+
+    @Override
+
+    public boolean onMarkerClick(Marker marker) {
+
+        final String address = (String) marker.getTitle();
+
+        input_search = root.findViewById(R.id.input_search);
+
+        input_search.setText(address);
+
+        String park_address = input_search.getText().toString();
+
+
+
+        if(input_search!=null){
+
+            Intent i = new Intent(getContext(),DetailsActivity.class);
+
+            i.putExtra("address",park_address);
+
+            startActivity(i);
+
+        }
+
+
+
+        return false;
+
+    }
 
 }
