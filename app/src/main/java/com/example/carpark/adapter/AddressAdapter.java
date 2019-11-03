86440586c2b1226d6a4da278f7fcdd8619bd8636
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2019-11-03 03:59:04 +0100

    Added the Map and Address ListView when Schedule is clicked to match current design

--- app/src/main/java/com/example/carpark/adapter/AddressAdapter.java
+++ app/src/main/java/com/example/carpark/adapter/AddressAdapter.java
@@ -1,16 +1,19 @@
 package com.example.carpark.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.carpark.Model.ParkAddress;
 import com.example.carpark.Model.Vehicle;
 import com.example.carpark.R;
+import com.example.carpark.views.ScheduleActivity;
 
 import java.util.List;
 
@@ -28,13 +31,14 @@ public class AddressAdapter extends RecyclerView.Adapter<AddressAdapter.CustomVi
         public final View view;
         TextView address_name;
         TextView park_name;
-
+        CardView park_details;
         CustomViewHolder(View itemView) {
             super(itemView);
             view = itemView;
 
             address_name = view.findViewById(R.id.park_address);
             park_name = view.findViewById(R.id.park_name);
+            park_details = view.findViewById(R.id.park_location);
         }
     }
 
@@ -47,9 +51,20 @@ public class AddressAdapter extends RecyclerView.Adapter<AddressAdapter.CustomVi
 
     @Override
     public void onBindViewHolder(CustomViewHolder holder, int position) {
-
+        final String park_name = parkAddresses.get(position).getPark_name();
+        final String park_address = parkAddresses.get(position).getPark_address();
         holder.address_name.setText(parkAddresses.get(position).getPark_address());
         holder.park_name.setText(parkAddresses.get(position).getPark_name());
+        holder.park_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, ScheduleActivity.class);
+                i.putExtra("Park_Name",park_name);
+                i.putExtra("Park_Address",park_address);
+                context.startActivity(i);
+            }
+        });
+
     }
 
     @Override
