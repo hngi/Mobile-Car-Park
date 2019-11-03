995c387436173608abfa041f336e47b3bd522393
Zainab Jimoh <jimohzainab4753@gmail.com>
2019-11-03 22:51:51 +0000

    Allowed user to edit name and other details on the Edit Profile page and update it in the API.

--- app/src/main/java/com/example/carpark/Api/ParkingApi.java
+++ app/src/main/java/com/example/carpark/Api/ParkingApi.java
@@ -17,9 +17,11 @@ import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ParkingApi {
@@ -84,7 +86,7 @@ public interface ParkingApi {
     Call<User> getUserProfile();
 
     @Headers({"Accept:application/json","Content-Type:application/json"})
-    @PATCH("user")
-    Call<BaseDataResponse<UserProfile>> editUserProfile(@Body UserProfile userProfile);
+    @PUT("user")
+    Call<BaseDataResponse<UserProfile>> editUserProfile(@Body UserProfile userProfile,@Header("Authorization") String token);
 
 }
